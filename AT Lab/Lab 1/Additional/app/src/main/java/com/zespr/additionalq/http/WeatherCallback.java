8b7438b936cbf0b15333a62d7f53671c4a376a43
nikeplusdash <nikeplusdash@gmail.com>
2021-02-26 11:43:42 +0530

    Update

--- AT Lab/Lab 1/Additional/app/src/main/java/com/zespr/additionalq/http/WeatherCallback.java
+++
@@ -1,8 +0,0 @@
-package com.zespr.additionalq.http;
-
-import java.util.List;
-
-public interface WeatherCallback {
-    void onSuccess(Result[] weather);
-    void onError();
-}
---
+++ README.md
@@ -0,0 +1,5 @@
+(AT Lab)[https://github.com/nikeplusdash/Semester-VI-Labs/tree/AT]
+
+(DWDM Lab)[https://github.com/nikeplusdash/Semester-VI-Labs/tree/DWDM]
+
+(ITT Lab)[ITT]
